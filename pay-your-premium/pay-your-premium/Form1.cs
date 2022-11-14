33cb6d584c0d58992dd484d138392be79329c563
ahmed <am8150688@gmail.com>
2022-11-14 14:37:50 +0200

    update

--- pay-your-premium/pay-your-premium/Form1.cs
+++ pay-your-premium/pay-your-premium/Form1.cs
@@ -16,5 +16,10 @@ namespace pay_your_premium
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
