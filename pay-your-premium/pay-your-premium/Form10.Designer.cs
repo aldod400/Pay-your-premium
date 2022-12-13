09d244d492e3f3466c7b7f88acdadc11e9944e7c
Abdelrahman Elghonemy <aldodelghonemy400@gmail.com>
2022-12-13 17:28:08 +0200

    updated

--- pay-your-premium/pay-your-premium/Form10.Designer.cs
+++ pay-your-premium/pay-your-premium/Form10.Designer.cs
@@ -31,7 +31,6 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clients));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -42,10 +41,17 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.Back = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Back)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -64,8 +70,8 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(55)))), ((int)(((byte)(147)))));
-            this.button1.Location = new System.Drawing.Point(449, 559);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(16, 583);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(325, 37);
             this.button1.TabIndex = 16;
@@ -80,13 +86,13 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.White;
             this.groupBox1.Location = new System.Drawing.Point(11, 75);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(1173, 461);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(1173, 351);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Contract";
+            this.groupBox1.Text = "Contracts";
             // 
             // dataGridView1
             // 
@@ -106,25 +112,17 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.ColumnHeadersHeight = 35;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.Location = new System.Drawing.Point(5, 21);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
-            this.dataGridView1.Size = new System.Drawing.Size(1163, 435);
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(1163, 303);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
@@ -133,7 +131,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(974, 23);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(155, 23);
             this.textBox1.TabIndex = 13;
@@ -144,7 +142,7 @@
             this.close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.close.Image = global::pay_your_premium.Properties.Resources.error;
             this.close.Location = new System.Drawing.Point(1170, 2);
-            this.close.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.close.Margin = new System.Windows.Forms.Padding(2);
             this.close.Name = "close";
             this.close.Size = new System.Drawing.Size(22, 23);
             this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -156,7 +154,7 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
             this.panel3.Location = new System.Drawing.Point(45, 50);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(262, 3);
             this.panel3.TabIndex = 37;
@@ -171,7 +169,7 @@
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(55)))), ((int)(((byte)(147)))));
             this.button3.Location = new System.Drawing.Point(1107, 25);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(21, 19);
             this.button3.TabIndex = 38;
@@ -192,13 +190,74 @@
             this.Back.TabStop = false;
             this.Back.Click += new System.EventHandler(this.Back_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(55)))), ((int)(((byte)(147)))));
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(11, 430);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(1173, 142);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Contract";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(5, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1163, 108);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(55)))), ((int)(((byte)(147)))));
+            this.button2.Location = new System.Drawing.Point(437, 583);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(325, 37);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "PRINT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(55)))), ((int)(((byte)(147)))));
+            this.button4.Location = new System.Drawing.Point(845, 583);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(325, 37);
+            this.button4.TabIndex = 41;
+            this.button4.Text = "CLEAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Clients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::pay_your_premium.Properties.Resources.R__11_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1195, 622);
+            this.ClientSize = new System.Drawing.Size(1198, 631);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.Back);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.panel3);
@@ -209,7 +268,7 @@
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Clients";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form10";
@@ -218,6 +277,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Back)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,5 +294,11 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox Back;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button4;
     }
 }
--- pay-your-premium/pay-your-premium/Form10.cs
+++ pay-your-premium/pay-your-premium/Form10.cs
@@ -89,30 +89,40 @@ namespace pay_your_premium
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DataGridViewRow row = new DataGridViewRow();
-            row = dataGridView1.Rows[selectedRowIndex];
-            int iid = int.Parse(row.Cells[0].Value.ToString());
-            string client_name = Interaction.InputBox("Enter The New Client Name", "Update Data", row.Cells[1].Value.ToString(), -1, -1);
-            string national_number = Interaction.InputBox("Enter The New National Number", "Update Data", row.Cells[2].Value.ToString(), -1, -1);
-            string address  = Interaction.InputBox("Enter The New Address", "Update Data", row.Cells[3].Value.ToString(), -1, -1);
-            string date_birth  = Interaction.InputBox("Enter The New Date Birth", "Update Data", row.Cells[4].Value.ToString(), -1, -1);
-            string device_description  = Interaction.InputBox("Enter The Divice Description", "Update Data", row.Cells[5].Value.ToString(), -1, -1);
-            string total_price = Interaction.InputBox("Enter The New Total Price", "Update Data", row.Cells[6].Value.ToString(), -1, -1);
-            string deposit = Interaction.InputBox("Enter The New Deposit", "Update Data", row.Cells[7].Value.ToString(), -1, -1);
-            string premium_period  = Interaction.InputBox("Enter The Premium Period", "Update Data", row.Cells[8].Value.ToString(), -1, -1);
-            string start_date  = Interaction.InputBox("Enter The New Start Date", "Update Data", row.Cells[9].Value.ToString(), -1, -1);
-            string end_date = Interaction.InputBox("Enter The New End Date", "Update Data", row.Cells[10].Value.ToString(), -1, -1);
-            cn.Open();
-            SqlCommand cm = new SqlCommand("UPDATE [dbo].[contracts] SET client_name = '"+ client_name + "', national_number = '"+ national_number + "', address = '"+ address + "', date_birth = '"+ date_birth + "', device_description = '"+ device_description + "', total_price = '"+ total_price + "', deposit = '"+ deposit + "', premium_period = '"+ premium_period + "', start_date = '"+ start_date + "',end_date = '"+ end_date + "' WHERE contracts_id = '"+ iid + "'", cn);
-            cm.ExecuteNonQuery();
-            cn.Close();
-            SqlDataAdapter da;
-            DataTable dt = new DataTable();
-            cn.Open();
-            da = new SqlDataAdapter("select*from contracts", cn);
-            da.Fill(dt);
-            dataGridView1.DataSource = dt;
-            cn.Close();
+            try
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row = dataGridView1.Rows[selectedRowIndex];
+                int iid = int.Parse(row.Cells[0].Value.ToString());
+                string client_name = Interaction.InputBox("Enter The New Client Name", "Update Data", row.Cells[1].Value.ToString(), -1, -1);
+                string national_number = Interaction.InputBox("Enter The New National Number", "Update Data", row.Cells[2].Value.ToString(), -1, -1);
+                string address = Interaction.InputBox("Enter The New Address", "Update Data", row.Cells[3].Value.ToString(), -1, -1);
+                string date_birth = Interaction.InputBox("Enter The New Date Birth", "Update Data", row.Cells[4].Value.ToString(), -1, -1);
+                string device_description = Interaction.InputBox("Enter The Divice Description", "Update Data", row.Cells[5].Value.ToString(), -1, -1);
+                string total_price = Interaction.InputBox("Enter The New Total Price", "Update Data", row.Cells[6].Value.ToString(), -1, -1);
+                string deposit = Interaction.InputBox("Enter The New Deposit", "Update Data", row.Cells[7].Value.ToString(), -1, -1);
+                string premium_period = Interaction.InputBox("Enter The Premium Period", "Update Data", row.Cells[8].Value.ToString(), -1, -1);
+                string start_date = Interaction.InputBox("Enter The New Start Date", "Update Data", row.Cells[9].Value.ToString(), -1, -1);
+                string end_date = Interaction.InputBox("Enter The New End Date", "Update Data", row.Cells[10].Value.ToString(), -1, -1);
+                cn.Open();
+                SqlCommand cm = new SqlCommand("UPDATE [dbo].[contracts] SET client_name = '" + client_name + "', national_number = '" + national_number + "', address = '" + address + "', date_birth = '" + date_birth + "', device_description = '" + device_description + "', total_price = '" + total_price + "', deposit = '" + deposit + "', premium_period = '" + premium_period + "', start_date = '" + start_date + "',end_date = '" + end_date + "' WHERE contracts_id = '" + iid + "'", cn);
+                cm.ExecuteNonQuery();
+                cn.Close();
+                SqlDataAdapter da;
+                DataTable dt = new DataTable();
+                cn.Open();
+                da = new SqlDataAdapter("select*from contracts", cn);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                cn.Close();
+
+                richTextBox1.Text = string.Format("                               Premium Contract                               \n\n\n\n This  contract  encloses  terms  and  conditions  with  which  a    contractor : Pay your premium  will  offer  services  to " + "\n\n - Client :  " + client_name + " ,   \n\n - Client Nation Number :" + iid.ToString() + "\n\n - Adreess : " + address + "\n\n - Day Of Birth : " + date_birth + "\n\n\n The contractor shall: \n\n - begin work on " + start_date + "\n\n - Complete on  " + end_date + " \n\n\n The Product Details : \n " + device_description + " \n\n - The Total Price : " + total_price + "\n\n - Depodit : " + deposit + "\n\n - Premiumm Period : " + premium_period);
+            }
+            catch (Exception Message)
+            {
+                MessageBox.Show("Please Select the Row to Update");
+            }
+           
         }
 
         private void button3_Click_1(object sender, EventArgs e)
@@ -130,6 +140,21 @@ namespace pay_your_premium
             Main main = new Main();
             main.Show();
             this.Hide();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(richTextBox1.Text, new Font("Sans Serif", 8), Brushes.Black, 10, 20);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            printDialog1.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
         }
     }
 }
